1522cffc0760a658907c0776d4b2d269662601fe
Christoph Kehl <saudumm1@googlemail.com>
2020-11-08 15:29:38 +0100

    v1.1.0 - changed to cache dir

--- News-Widget-Clear-Cache.js
+++ News-Widget-Clear-Cache.js
@@ -2,17 +2,17 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: red; icon-glyph: trash-alt;
 /* DELETE CACHE OF SCRIPTABLE NEWS WIDGET
- v1.0.0 coded by Saudumm (https://twitter.com/saudumm)
+ v1.1.0 coded by Saudumm (https://twitter.com/saudumm)
  GitHub: https://github.com/Saudumm/scriptable-News-Widget
 */
 
 let fm = FileManager.local();
-let dir = fm.documentsDirectory();
+let dir = fm.cacheDirectory();
 let alertMsg = "No files to delete";
 
-if (fm.fileExists(dir+"/saudumm-widget-news-data")) {
+if (fm.fileExists(dir+"/saudumm-news-widget-data")) {
   try {
-    fm.remove(dir+"/saudumm-widget-news-data");
+    fm.remove(dir+"/saudumm-news-widget-data");
     alertMsg = "All cache files deleted!";
   } catch(err) {
     alertMsg = "ERROR while deleting cache files";
@@ -23,5 +23,3 @@ let alert = new Alert();
 alert.message = "\n"+alertMsg;
 alert.title = "Clear News Widget Cache";
 alert.presentAlert();
-
-// end of script
